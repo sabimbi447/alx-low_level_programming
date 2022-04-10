752676450e0b56757001547a77ee3b5d1532efbe
sabimbi447 <asarejonas447@gmail.com>
2022-04-10 14:29:06 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (success/correct)
+  */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10, digit++)
+{
+printf("%d digit", digit);
+}
+printf("\n");
+
+return (0);
+}
