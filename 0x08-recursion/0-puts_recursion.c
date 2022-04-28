6be57a4ccf11e065d0ee76ad71319c7640dab63f
sabimbi447 <asarejonas447@gmail.com>
2022-04-28 15:19:39 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+  *_putchar_recursion - prints a string
+  *@s: pointer block of memory to fill
+  *Return: void
+  */
+
+
+void _puts_recursion(char *s)
+{
+	if ( *s == '\0')
+		_putchar('\n');
+	else 
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+}
+
