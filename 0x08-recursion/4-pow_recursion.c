7aa21a56e021379127385ffc59182a4c33c75b19
sabimbi447 <asarejonas447@gmail.com>
2022-04-28 19:19:12 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-  * _pow_recursion - Calcate the expontient of a number
-  * @x: base
-  * @y: expontient
-  * Return: int
-  */
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
 
 int _pow_recursion(int x, int y)
 {
+
 	if (y < 0)
 		return (-1);
 	else if (y == 1)
@@ -16,6 +18,7 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 
-	return (x * _pow_recursion (x, y -1));
+	return (x * _pow_recursion(x, y - 1));
+
 }
 
