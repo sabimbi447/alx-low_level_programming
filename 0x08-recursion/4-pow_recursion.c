22468db76f85b16524690ba9d28571fe311e4230
sabimbi447 <asarejonas447@gmail.com>
2022-04-28 19:10:18 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - Calcate the expontient of a number
+  * @x : base
+  * @y : expontient
+  * Return: int
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y -1));
+}
