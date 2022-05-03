fcf985b47d13baced3f406c4c69f26ab086357f7
sabimbi447 <asarejonas447@gmail.com>
2022-05-02 19:05:20 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - print name of program
+  * @argc: argument count
+  * @argv: arguemnt vector
+  * Return: 0
+  */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
