1cab6bf1db080a36e81fa75ac1cf697ab6c2d433
sabimbi447 <asarejonas447@gmail.com>
2022-05-02 19:30:52 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,16 +10,11 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	if (argc > 0)
+	
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 0; i <= argc -1; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
 	
-	else
-		printf("There only one argument");
-
 	return (0);
 }
