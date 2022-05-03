804ed9820f79b862feb40bf4a277364311fefa35
sabimbi447 <asarejonas447@gmail.com>
2022-05-02 19:00:35 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - function
+  * @argc: argument count
+  * @argv: argument vlaue
+  * Return: 0
+  */
+
+
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
