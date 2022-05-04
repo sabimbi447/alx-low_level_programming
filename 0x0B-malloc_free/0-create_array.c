938c71674422052fc5f41a7ee55b9b4d16e5c823
sabimbi447 <asarejonas447@gmail.com>
2022-05-04 06:32:23 -0700

    Fix bug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,16 @@
   */
 
 char *create_array(unsigned int size, char c)
-{	
+{
 	char *ptr;
 	unsigned int position;
 
 	if (size == 0)
+	{
 		return (NULL);
-
+	}
+	
+	/*Define values with malloc*/
 	ptr = (char *) malloc(size * sizeof(c));
 
 	if (ptr == 0)
