b456b0550eef89c9b337aee4a8fa34518024b5e6
sabimbi447 <asarejonas447@gmail.com>
2022-05-09 00:04:30 -0700

    dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+  * struct dog - define a struct of dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner's identity
+  * Return: Always 0.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
